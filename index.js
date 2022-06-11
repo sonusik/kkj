672f5ef30cf41937ddd89a5e0e1dbb65bf6daf23
Pavlovskaya Sofya Alexandrovna <103187966+sonusik@users.noreply.github.com>
2022-06-11 13:01:26 +0500

    pr10

---
+++ index.js
@@ -0,0 +1,131 @@
+//Zadanie 8.1(a)
+/*
+let a="";
+for(let i=1;i!=6;i++)
+{
+a+="5"
+}
+for(let i=1;i!=6;i++)
+{
+console.log(a)
+}
+*/
+//Zadanie 8.1(b)
+/*
+let b="";
+for(let i=1;i!=11;i++)
+{
+b=b+i+" "
+}
+for(let i=1;i!=5;i++)
+{
+console.log(b)
+}
+*/
+//Zadanie 8.1(v)
+/*
+for(let i=4;i!=8;i++)
+{
+ for(let b=1;b!=10;b++)
+{
+process.stdout.write(i+""+j+" ")
+}
+console.log()
+}
+*/
+//Zadanie 8.2(a)
+/*
+let s="";
+for(let i=1;i!=6;i++)
+{
+s+="5"
+console.log(s)
+}
+*/
+//Zadanie 8.2(b)
+/*
+let a="";
+for(let i=6;i!=1;i--)
+{
+for(let h=1;h!=i;h++)
+{
+process.stdout.write("1 ")
+}
+console.log()
+}
+*/
+//Zadanie 8.3(a)
+/*
+let c="";
+for(let i=1;i!=6;i++)
+{
+for(let h=1;h<=i;h++)
+{
+process.stdout.write(i+" ")
+}
+console.log()
+}
+*/
+
+//Zadanie 8.3(b)
+/*
+let c="";
+let b=10;
+for(let i=6;i!=1;i--)
+{
+for(let h=1;h<=i;h++)
+{
+process.stdout.write(b+" ")
+}
+console.log()
+b++;
+}
+*/
+
+//Zadanie 8.3(v)
+/*
+let c="";
+let k=10;
+for(let i=1;i!=6;i++)
+{
+for(let h=1;h<=i;h++)
+{
+process.stdout.write(k+" ")
+}
+console.log()
+k+=10;
+}
+*/
+
+//Zadanie 8.3(g)
+/*
+let c="";
+let z=5;
+for(let i=6;i!=1;i--)
+{
+for(let h=1;h<=i;h++)
+{
+process.stdout.write(z+" ")
+}
+console.log()
+z+=5;
+}
+*/
+
+//Zadanie 10
+
+function del(a)
+{
+let count=0;
+for(let i=1;i<=9;i++)
+{
+if(a%i==0){count++}
+}
+{
+return count;
+}
+for(let i=1;i<=300;i++)
+{
+	if(del(i)==5)(console.log(i))
+}
+}
